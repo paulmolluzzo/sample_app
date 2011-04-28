78db5979f920e9964843a403141a13ff7e6d2e04
Paul Molluzzo <pmolluzzo@gmail.com>
2011-04-27 23:32:02 -0400

    Users DB

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,12 @@
 
 class User < ActiveRecord::Base
   attr_accessible :name, :email
+  
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates_presence_of :name
+  validates_length_of :name, :maximum =>50
+  validates_presence_of :email
+  validates_format_of :email, { :with => email_regex }
+  validates_uniqueness_of :email, { :case_sensitive => false }
 end
