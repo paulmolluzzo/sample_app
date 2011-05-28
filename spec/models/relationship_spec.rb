0b2950198bcb997b4a2601b9b944b5a72d4da29d
Paul Molluzzo <pmolluzzo@gmail.com>
2011-05-27 20:35:04 -0400

    Working follow & unfollow buttons (without Ajax working)

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,17 +1,20 @@
 require 'spec_helper'
 
 describe Relationship do
+  
   before(:each) do
     @follower = Factory(:user)
     @followed = Factory(:user, :email => Factory.next(:email))
+    
     @attr = { :followed_id => @followed.id }
   end
-
+  
   it "should create a new instance with valid attributes" do
     @follower.relationships.create!(@attr)
   end
   
   describe "follow methods" do
+    
     before(:each) do
       @relationship = @follower.relationships.create!(@attr)
     end
@@ -23,17 +26,18 @@ describe Relationship do
     it "should have the right follower" do
       @relationship.follower.should == @follower
     end
-    
+
     it "should have a followed attribute" do
       @relationship.should respond_to(:followed)
     end
-
+    
     it "should have the right followed user" do
       @relationship.followed.should == @followed
     end
   end
   
   describe "validations" do
+    
     it "should require a follower id" do
       Relationship.new(@attr).should_not be_valid
     end
